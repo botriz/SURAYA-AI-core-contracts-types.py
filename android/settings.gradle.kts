637bf7e660b80081fe98f31ea6c510d42136e4e6
Ahmad <313136093+botriz@users.noreply.github.com>
2026-09-22 05:57:21 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     repositories {
         google()
@@ -7,7 +9,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
 
     repositories {
         google()
@@ -16,4 +20,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "SurayaAI"
+
 include(":app")
