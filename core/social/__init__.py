96797e4014770b8651ea66c448c800d59da3b78e
Ahmad <313136093+botriz@users.noreply.github.com>
2026-09-22 04:23:07 +0330

    Create __init__.py

---
+++ core/social/__init__.py
@@ -0,0 +1,9 @@
+from core.social.base import SocialAccount, SocialPost, SocialProvider
+from core.social.manager import SocialManager
+
+__all__ = [
+    "SocialAccount",
+    "SocialPost",
+    "SocialProvider",
+    "SocialManager",
+]
